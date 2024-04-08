6df963146dfac356f3bfe07047a3353451a0e7d1
OZ <oz@livepigeon.com>
2024-04-08 20:56:53 +0500

    worked on front end api

--- traino_local/settings.py
+++ traino_local/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'traino_local.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db/db.sqlite3',
     }
 }
 
