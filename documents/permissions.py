9bec5719776bbb286cf374e3145a20f2faba18d2
Muhammad Jawwad turabi <jawwadturabi@gmail.com>
2024-05-16 19:27:52 +0500

    Documents Super Admin Permissions Re Write as Extras in Permissions.py file

--- documents/permissions.py
+++ documents/permissions.py
@@ -17,8 +17,9 @@ class IsAdminUserOrReadOnly(permissions.BasePermission):
         """
         # Read permissions are allowed for any request,
         # so we'll always allow GET, HEAD, or OPTIONS requests
-        # if request.method in permissions.SAFE_METHODS:
-        #     return True
+        if (request.user and request.user.is_authenticated and request.user.role == 'Super Admin')or (request.user and request.user.is_authenticated and request.user.role == 'User'):#
+            if request.method in permissions.SAFE_METHODS:
+                return True
         
         # # Write permissions are only allowed if the user is an admin and the object's company is the same as the user's
         return request.user.role == 'Admin'
